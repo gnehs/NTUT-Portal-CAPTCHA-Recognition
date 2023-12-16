eb5adced275bc63055e928b3d5447b0b2528c818
gnehs <jayuiop@gmail.com>
2023-12-17 00:00:45 +0800

    added onnx format

---
+++ export_onnx.py
@@ -0,0 +1,29 @@
+import torch
+from train import Net
+
+
+class Convert(object):
+    def __init__(self, model_path, gpu=True):
+        self.net = Net(gpu)
+        self.net.load(model_path)
+
+    def convert_to_onnx(self):
+
+        # Set the model to evaluation mode
+        self.net.eval()
+
+        # Export the model to ONNX
+        torch.onnx.export(self.net,                    # PyTorch Model
+                          torch.rand(1, 3, 39, 135),   # Input tensor
+                          "./model.onnx",              # ONNX file path
+                          verbose=True,
+                          # Input tensor name
+                          input_names=['input'],
+                          # Output tensor name
+                          output_names=['output'],
+                          dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}})
+
+
+if __name__ == '__main__':
+    man = Convert('pretrained')
+    man.convert_to_onnx()
